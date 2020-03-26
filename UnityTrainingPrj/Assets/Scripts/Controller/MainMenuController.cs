f17d0fadd9c78511dcec16235c90f10635e47e85
Nguyen Phuoc Thanh <nguyenphuocthanh.dn@gmail.com>
2020-03-26 11:44:01 +0700

    +Add gitignore +Add 2 scene:BasicGUI, TagAndLayer +Add examples to scene TagAndLayer

---
+++ UnityTrainingPrj/Assets/Scripts/Controller/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadSceneBasicGUI()
+    {
+        SceneManager.LoadScene("BasicGUI");
+    }
+    public void LoadSceneTagAndLayer()
+    {
+        SceneManager.LoadScene("TagAndLayer");
+    }
+}
---
+++ UnityTrainingPrj/Assets/Scripts/Ground.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("DestroyedByCollisionWithGround"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
