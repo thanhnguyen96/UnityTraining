f5eb023dce8ac18360599bc25fc189c4f757c28a
Nguyen Phuoc Thanh <nguyenphuocthanh.dn@gmail.com>
2020-04-01 16:30:08 +0700

    +Add some scripts and sprites

---
+++ UnityTrainingPrj/Assets/Scripts/UI/ForceBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForceBar : MonoBehaviour
+{
+    public Image fillBar;
+
+    public void Reset()
+    {
+        fillBar.fillAmount = 0;
+    }
+    public void setFillAmount(float amount)
+    {
+        fillBar.fillAmount = amount;
+    }
+}
